13843dbbe124f32ef663dcd4db5bd1b3b1867006
ttiurt <truittzach33@gmail.com>
2023-08-13 19:00:33 -0400

    onClick redirect add

--- src/components/ThreeTest/ThreeTest.jsx
+++ src/components/ThreeTest/ThreeTest.jsx
@@ -7,13 +7,18 @@ import { Canvas } from "@react-three/fiber"
 
 
 const ThreeTest = () => {
+
+  const handleClick = url => {
+    window.open(url, '_blank', 'noopener,noreferrer');
+  }
+
   return (
     <div className={styles.ThreeCntnr}>
       <Canvas>
         <OrbitControls enableZoom={false} autoRotate/>
         <ambientLight intensity={1}/>
         <directionalLight position={[3, 2, 1]}/>
-        <mesh>
+        <mesh onClick={() => handleClick('https://github.com/ttiurt')}>
           <boxGeometry args={[5, 2.5, .05]}/>
           <meshStandardMaterial color="white"/>
         </mesh>
